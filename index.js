2e0d24eecd688ef886e1acf3c393ace61c288e50
Erice1221 <ericesru@gmail.com>
2023-01-20 10:53:27 -0500

    test3

---
+++ index.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const app= express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "src")));    //server css file as static otherwise css won't be seen in index.html when sent through sendFile()
+app.use(express.json());
+//handling get request for home route
+app.get("/",function(req,res){     
+  res.sendFile(__dirname+"/src/main.html");
+});
