0db5513ffa7bda4b5a910b204d288e396f57f2be
Erice1221 <ericesru@gmail.com>
2023-01-24 19:49:25 -0500

    should be done

--- src/client.js
+++ src/client.js
@@ -1,7 +1,9 @@
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
+    
     ready()
+    
 }
 
 function ready() {
@@ -18,7 +20,11 @@ function ready() {
         .then(function (session) {
             
             document.body.style.backgroundColor=session.color
-            
+            if (session.color=="green" || session.color=="red"){
+                document.getElementById('main-status-id').innerText="Error"
+            } else {
+            document.getElementById('main-status-id').innerText=""
+            }
          
         
         })
@@ -30,13 +36,19 @@ function ready() {
         .catch(function (error) {
           console.error("Error:", error);
         });
+    
     document.getElementById('main-button-id').addEventListener('click', Changelightstate) 
     
 }
 
-
+var count=0
+var buttonstate=true
 function Changelightstate(){
-    if (document.body.style.backgroundColor=="rgb(252, 238, 167)" || document.body.style.backgroundColor=="rgb(53, 54, 58)"){
+    if ((document.body.style.backgroundColor=="rgb(252, 238, 167)" || document.body.style.backgroundColor=="rgb(53, 54, 58)") && buttonstate){
+        buttonstate=false
+        count=0
+       
+        
     fetch("/changelight", {
         headers: {'Content-Type': 'application/json'},
         method: "POST",
@@ -50,6 +62,8 @@ function Changelightstate(){
         .then(function (session) {
           
             document.body.style.backgroundColor=session.color
+            buttonstate=true
+            document.getElementById('main-status-id').innerText=""
             
          
         
@@ -62,7 +76,23 @@ function Changelightstate(){
         .catch(function (error) {
           console.error("Error:", error);
         });
-    } else {
+    } else if (!buttonstate){
+            console.log(count)
+
+            if(count==0) {
+                document.getElementById('main-status-id').innerText="wait"
+            } else if(count==1){
+                document.getElementById('main-status-id').innerText="WAITTT"    
+            } else if(count==2){
+                document.getElementById('main-status-id').innerText="I SAID WAIT"    
+            } else if(count>2){
+                document.getElementById('main-status-id').innerText="You don't know what wait means???"    
+                
+            } 
+            count++
+        }else{     
         document.getElementById('main-status-id').innerText="Wait For the Background to be yellow or black"
     }
+
+   
 }
